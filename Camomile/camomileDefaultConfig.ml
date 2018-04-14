66752a493fe140e542606d153e88e9058154406c
yoriyuki <yoriyuki.y@gmail.com>
2018-04-15 01:19:22 +0900

    Moving around files

--- Camomile/camomileDefaultConfig.ml
+++
@@ -1,4 +0,0 @@
-let datadir    = Filename.concat InstallConfig.share_dir "database"
-let localedir  = Filename.concat InstallConfig.share_dir "locales"
-let charmapdir = Filename.concat InstallConfig.share_dir "charmaps"
-let unimapdir  = Filename.concat InstallConfig.share_dir "mappings"
